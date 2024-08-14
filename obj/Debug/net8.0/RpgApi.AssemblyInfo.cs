8ee7927d692fa3e6cbd29c47f2a4cfc48ea314c9
Diego Matheus <dms.diego.2019@gmail.com>
2024-08-13 22:14:09 -0300

    criado conexao banco no azure

--- obj/Debug/net8.0/RpgApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/RpgApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RpgApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa990c76d3a78f5c85bcbe3ea1e3c3586471186f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad56c21b4d37220ec5b1806c855f6b3a341c0b9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("RpgApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RpgApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
